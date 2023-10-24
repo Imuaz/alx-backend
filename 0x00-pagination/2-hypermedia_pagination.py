96e7a237fc11f06b87641ce9ea8bb20ae63e3354
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-24 10:48:38 +0100

    hyper method

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,7 +51,7 @@ class Server:
         total_pages = math.ceil(len(self.dataset()) / page_size)
 
         # Calculate next and previous page number
-        next_page = (page + 1) if page < total_pages else None
+        next_page = (page + 1) if len (page_data) > 0 else None
         prev_page = (page - 1) if page > 1 else None
         # Create the dictionary
         hyper_dict = {
