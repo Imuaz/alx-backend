e2094a81fd54e792bc9e4f537a021cf4140f8d81
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-27 21:39:48 +0100

    hypermedia implemented

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,7 +44,7 @@ class Server:
             return []
         return dataset[page_range[0]: page_range[1]]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
         """Return hypermedia of a page."""
         total_records = len(self.dataset())
         total_pages = math.ceil(total_records / page_size)
