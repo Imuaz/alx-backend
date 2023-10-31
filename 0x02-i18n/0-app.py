0651146020c15ac01eaec76bfb31558e3ec4b9bf
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-31 19:54:42 +0100

    repo set up and task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index() -> str:
     '''index home page'''
     return render_template('0-index.html')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=500, debug=True)
